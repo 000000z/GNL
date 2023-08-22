ecdf01779300ff6f5812a9564be3376095640a95
000000 <106448541+000000z@users.noreply.github.com>
2023-08-22 13:30:34 +0200

    Add files via upload

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,24 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adzinabi <adzinabi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: adzinabi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/04 14:23:22 by adzinabi          #+#    #+#             */
-/*   Updated: 2023/07/04 14:23:22 by adzinabi         ###   ########.fr       */
+/*   Created: 2023/07/13 23:05:25 by adzinabi          #+#    #+#             */
+/*   Updated: 2023/07/13 23:05:26 by adzinabi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-size_t  ft_strlen(const char *str)
+#include "get_next_line.h"
+
+int	ft_strclen(char *str, char c)
 {
-    size_t  i;
+	int	len;
 
-    i = 0;
-
-    if(str)
-    {
-        while(str[i])
-            i++;
-        return(i);
-    }
-    return(0);
+	len = 0;
+	if (!str)
+		return (0);
+	while (str[len] && str[len] != c)
+		len++;
+	return (len);
 }
